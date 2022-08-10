ba2351e0526f9c6ad602b015cbdb51ea3ad16098
stanislak <gunmanmain@gmail.com>
2022-08-11 00:28:11 +0300

    Вносит правки наставника

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ const server = (done) => {
 
 const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
-  gulp.watch('mpmsource/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html').on('change', browser.reload);
 }
 
 
