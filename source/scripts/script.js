2a3d3c13e8fc8aca4277077accfb912310364579
stanislak <gunmanmain@gmail.com>
2022-08-25 20:14:30 +0300

    Добавляет верстку адаптивных сеток мелких блоков

---
+++ source/scripts/script.js
@@ -0,0 +1,15 @@
+let navMain = document.querySelector('.main-nav__toggle');
+let mainNav = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (mainNav.classList.contains('main-nav--closed')) {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
+  } else {
+    mainNav.classList.add('main-nav--closed');
+    mainNav.classList.remove('main-nav--opened');
+  }
+});
